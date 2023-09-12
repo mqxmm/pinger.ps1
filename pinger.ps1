390ff3fe8fa77eaad6a1de7f0162df5f5887a446
mqxmm <79595418+mqxmm@users.noreply.github.com>
2023-09-12 22:14:49 +0300

    Add files via upload

---
+++ pinger.ps1
@@ -0,0 +1,36 @@
+﻿param (
+    [string]$ip,
+    [int]$start,
+    [int]$end
+)
+
+if ($ip -and $start -and $end) {
+    $n = 0
+    $jobs = @()
+
+    for ($i = $start; $i -le $end; $i++) {
+        $address = "$ip.$i"
+        $pingResult = Test-Connection -ComputerName $address -Count 1 -ErrorAction SilentlyContinue -AsJob
+        $jobs += $pingResult
+    }
+
+    # Wait for all ping jobs to complete
+    $jobs | Wait-Job
+
+    # Check the results of each job
+    $jobs | ForEach-Object {
+        $pingResult = Receive-Job -Job $_
+        Remove-Job -Job $_
+        
+        if ($pingResult.StatusCode -eq 0) {
+            Write-Host "$($pingResult.Address) is up."
+            $n++
+        }
+    }
+
+    Write-Host ""
+    Write-Host "$n hosts are up."
+} else {
+    Write-Host "Usage: $PSCommandPath <ip> <start> <end>"
+    Write-Host "Error: Insufficient arguments!"
+}
